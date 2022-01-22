11c60d10da8be5e4ce083541a4b27abec08777e8
BrandonW <77559638+BrandonIW@users.noreply.github.com>
2022-01-22 11:16:24 -0800

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -13,10 +13,10 @@ plaintext = ["1001 0100 0110 0110",
              "1100 0010 0111 0100"]
 
 keys = ["1010 0010 0011 1010",
-        "1110 1111 0001 1000",]
+        "1110 1111 0001 1000"]
 
-sbox = [[15, 10, 2, 5, 8, 4, 11, 6, 1, 0, 14, 7, 9, 3, 12, 13],
-        [4, 0, 15, 10, 8, 9, 7, 13, 5, 1, 6, 11, 2, 3, 14, 12]]
+subboxes = [[15, 10, 2, 5, 8, 4, 11, 6, 1, 0, 14, 7, 9, 3, 12, 13],
+            [4, 0, 15, 10, 8, 9, 7, 13, 5, 1, 6, 11, 2, 3, 14, 12]]
 
 
 class WeakCipher:
@@ -24,26 +24,46 @@ class WeakCipher:
         self.keys = key_list
         self.plaintext = pt_list
         self.sbox = sbox_list
+        self.pt_stack = deque(self.plaintext)
+        self.bits_changed = 0
 
     def main(self):
         for key in self.keys:
-            self.encryption(deque(self.plaintext), key, self.sbox)
-        quit(0)
+            while self.pt_stack:
+                popped_plain = self.pt_stack.popleft()
+                self.encryption(popped_plain, key, self.sbox)
+                self.bits_changed += self.calc_avalanche(popped_plain, key, self.sbox)
+            self.pt_stack = deque(self.plaintext)
 
-    def encryption(self, ptext, ktext, sbox):
+        return f"Total Avalanche Effect: {self.bits_changed / 5120}"
+
+
+    def encryption(self, ptext, ktext, sbox, avalanche=False):
         order = [[3, 1, 4, 2], [1, 3, 2, 4]]
         sbox = cycle(sbox)
 
-        while ptext:
-            popped_plain = ptext.popleft()
-            p_text = [num for idex, num in sorted(zip(order[0], popped_plain.split(" ")))]
-            k_text = [num for idex, num in sorted(zip(order[1], ktext.split(" ")))]
+        p_text = [nibble for _, nibble in sorted(zip(order[0], ptext.split(" ")))]
+        k_text = [nibble for _, nibble in sorted(zip(order[1], ktext.split(" ")))]
 
-            xor = list(map(lambda nibble: bin(nibble)[2:].zfill(4), [
-                int(nib1, 2) ^ int(nib2, 2) for nib1, nib2 in zip(p_text, k_text)]))
-            post_sub = [self.sub_box(nibble, next(sbox)) for nibble in xor]
+        xor = list(map(lambda nibble: bin(nibble)[2:].zfill(4), [
+            int(nib1, 2) ^ int(nib2, 2) for nib1, nib2 in zip(p_text, k_text)]))
+        if avalanche:
+            return xor
 
-            self.write(popped_plain, xor, ktext, post_sub)
+        post_sub = [self.sub_box(nibble, next(sbox)) for nibble in xor]
+        self.write(ptext, xor, ktext, post_sub)
+
+    def calc_avalanche(self, ptext, ktext, sbox):
+        p_text_list, c_text_list = [ptext], []
+
+        for idex,num in enumerate(ptext):
+            if num != " ":
+                p_text_list.append(ptext[:idex] + str(1 - int(num)) + ptext[idex + 1:])
+
+        for plaintxt in p_text_list:
+            c_text_list.append(self.encryption(plaintxt, ktext, sbox, True))
+
+        return self.calc_average_avalanche(c_text_list[0], c_text_list[1:])
 
     @staticmethod
     def sub_box(nibble, sbox):
@@ -55,10 +75,16 @@ class WeakCipher:
     @staticmethod
     def write(plaintext, ciphertext, key, post_sub):
         with open("Assignment 1.txt", 'a') as file:
-            file.write(f"Plaintext: {plaintext} || Ciphertext Post-XOR: {ciphertext} || "
-                       f"Key Used: {key} || Ciphertext Post-Substitution: {post_sub}\n")
+            file.write(f"Plaintext: {plaintext} || Key Used: {key} || "
+                       f"Ciphertext Post-XOR: {ciphertext}  || Ciphertext Post-Substitution: {post_sub}\n")
 
+    @staticmethod
+    def calc_average_avalanche(ciphertext_original, ciphertext_changed):
+        count = 0
+        for ciphertext in ciphertext_changed:
+            count += sum(1 for bit1,bit2 in zip(ciphertext_original, ciphertext) if bit1 != bit2)
+        return count
 
 if __name__ == '__main__':
-    enc1 = WeakCipher(plaintext, keys, sbox)
-    enc1.main()
+    enc1 = WeakCipher(plaintext, keys, subboxes)
+    print(enc1.main())
