29118310ff1da099e5131ea1a6a059f49ca03e92
BrandonW <77559638+BrandonIW@users.noreply.github.com>
2022-01-31 14:06:19 -0800

    Update Encryption.py

--- Encryption.py
+++ Encryption.py
@@ -18,9 +18,8 @@ keys = ["1010 0010 0011 1010",
 subboxes_main = [[15, 10, 2, 5, 8, 4, 11, 6, 1, 0, 14, 7, 9, 3, 12, 13],
                  [4, 0, 15, 10, 8, 9, 7, 13, 5, 1, 6, 11, 2, 3, 14, 12]]
 
-
 subboxes_additional_diffusion = [[6, 12, 4, 5, 1, 14, 11, 9, 2, 0, 15, 3, 7, 8, 10, 13],
-                                 [1, 2, 15, 11, 14, 7, 4, 10, 5, 3, 6, 8, 0, 9, 13, 12]]
+                                 [1, 2, 15, 11, 14, 7, 4, 10, 5, 3, 6, 8, 9, 0, 13, 12]]
 
 
 class WeakCipher:
@@ -32,30 +31,35 @@ class WeakCipher:
         self.bits_changed = 0
 
     def main(self):
+        """ For each key, take our stack of plaintexts and pop off a single plaintext. Run that plaintext through
+        both self.encryption and self.bits_changed"""
+
         for key in self.keys:
             while self.pt_stack:
                 popped_plain = self.pt_stack.popleft()
                 self.encryption(popped_plain, key, self.sbox)
                 self.bits_changed += self.calc_avalanche(popped_plain, key, self.sbox)
             self.pt_stack = deque(self.plaintext)
-
         return f"Total Number of Changed Bits: {self.bits_changed}\nAvalanche Effect: {self.bits_changed / 5120}"
 
     def encryption(self, ptext, ktext, sbox, avalanche=False):
+        """ First reorder the nibbles of the plaintext and key so we can more easily zip together the nibbles that
+        will be XOR'd. Then XOR each paired/zipped nibble. Then run the resulting XOR'd binary through the sboxes """
+
         order = [[3, 1, 4, 2], [1, 3, 2, 4]]
         sbox_cycle = cycle(sbox)
 
-        p_text = [nibble for _, nibble in sorted(zip(order[0], ptext.split(" ")))] # Reorder nibbles to make it easier to zip and xor
+        p_text = [nibble for _, nibble in sorted(zip(order[0], ptext.split(" ")))]
         k_text = [nibble for _, nibble in sorted(zip(order[1], ktext.split(" ")))]
 
-        word_post_xor = list(map(lambda nibble: bin(nibble)[2:].zfill(4), [        # xor operation
-            int(nib1, 2) ^ int(nib2, 2) for nib1, nib2 in zip(p_text, k_text)])) 
+        word_post_xor = list(map(lambda nibble: bin(nibble)[2:].zfill(4), [
+            int(nib1, 2) ^ int(nib2, 2) for nib1, nib2 in zip(p_text, k_text)]))
 
-        post_sub_digits = [self.sub_box(nibble, next(sbox_cycle)) for nibble in word_post_xor]  # Ciphertext in Decimal Form
-        post_sub_binary = [bin(digit)[2:].zfill(4) for digit in post_sub_digits]                # Ciphertext in Binary Form
+        post_sub_digits = [self.sub_box(nibble, next(sbox_cycle)) for nibble in word_post_xor]
+        post_sub_binary = [bin(digit)[2:].zfill(4) for digit in post_sub_digits]
 
         if avalanche:
-            sbox_cycle2 = cycle(self.sbox2)                                                     # Additional Sbox to increase Avalanche
+            sbox_cycle2 = cycle(self.sbox2)
             post_sub_digits = [self.sub_box(nibble, next(sbox_cycle2)) for nibble in post_sub_binary]
             post_sub_binary = [bin(digit)[2:].zfill(4) for digit in post_sub_digits]
 
@@ -64,26 +68,31 @@ class WeakCipher:
         self.write(ptext, word_post_xor, ktext, post_sub_digits, post_sub_binary)
 
     def calc_avalanche(self, ptext, ktext, sbox):
+        """ For each bit and index in a given plaintext, flip the bit at that particular index and append the new
+        plaintext with the flipped bit to a list. Then run each of the 17 plaintexts (The original + 1 plaintext for each
+        flipped bit) through our encryption function and append to list of new ciphertexts. c_text_list[0] will be the
+         ciphertext of the original plaintext. Compare each of our other 16 CTs with index 0 and add up bits changed """
+        
         p_text_list, c_text_list = [ptext], []
 
         for idex, num in enumerate(ptext):
             if num != " ":
-                p_text_list.append(ptext[:idex] + str(1 - int(num)) + ptext[idex + 1:])  # Create a list of all plaintexts with 1 flipped bit
+                p_text_list.append(ptext[:idex] + str(1 - int(num)) + ptext[idex + 1:])
 
         for plaintxt in p_text_list:
-            c_text_list.append(self.encryption(plaintxt, ktext, sbox, True))             # Run each plaintext through encryption function and
-                                                                                         # append to list. c_text_list[0] is the original ciphertext
-        return self.calc_bits_changed(c_text_list[0], c_text_list[1:])                   # that we compare to calc. avalanche effect
+            c_text_list.append(self.encryption(plaintxt, ktext, sbox, True))
+
+        return self.calc_bits_changed(c_text_list[0], c_text_list[1:])
 
     @staticmethod
     def sub_box(nibble, sbox):
-        nested_sbox = [list(islice(sbox, idex, idex + 4))                                # Create nested lists out of sbox, so we can use x/y coords
+        nested_sbox = [list(islice(sbox, idex, idex + 4))
                        for idex in range(0, len(sbox), 4)]
         x_coord, y_coord = int(nibble[:2], 2), int(nibble[2:], 2)
         return nested_sbox[y_coord][x_coord]
 
     @staticmethod
-    def calc_bits_changed(ciphertext_original, ciphertext_changed):                      # Calculate number of changed bits per plaintext 
+    def calc_bits_changed(ciphertext_original, ciphertext_changed):
         count = 0
         ciphertext_original = "".join(ciphertext_original)
 
